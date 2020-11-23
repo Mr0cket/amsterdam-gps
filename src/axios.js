e38bba2c8c4ae3b93ad9cc93dbd3534d1f332a9c
Milo Silva <milo.silva234@gmail.com>
2020-11-23 14:16:43 +0100

    Refactor dataFetching to axios.js, implement Home

--- src/axios.js
+++ src/axios.js
@@ -1,5 +1,28 @@
 import axios from "axios";
 
-export default axios.create({
+axios.create({
   baseURL: "https://my-json-server.typicode.com/Codaisseur/patient-doctor-data",
 });
+async function fetchData(endPoint, setState) {
+  const doctorData = await axios
+    .get(endPoint)
+    .then((res) => res.data)
+    .catch((e) => console.error("request error:", e));
+
+  if ("0" in doctorData) {
+    console.log(doctorData);
+    setState({
+      data: doctorData,
+      status: "success",
+      error: false,
+    });
+  } else if (doctorData.length === 0) {
+    console.log("error retrieving data");
+    setState({
+      data: false,
+      status: "fail",
+      error: "error retrieving data",
+    });
+  } else console.log("I shouldnt get here... but I did :/");
+}
+export default fetchData;
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,10 +1,19 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Jumbotron } from "react-bootstrap";
 
 export default function Home() {
+  const isBusinessHours = () => {
+    const HoursOfDay = new Date().getHours();
+    console.log(HoursOfDay);
+    return true;
+  };
   return (
-    <Container>
-      <h1>Homepage</h1>
-    </Container>
+    <Jumbotron fluid>
+      <Container>
+        <h1>Amsterdam GPs</h1>
+        <h5>We are: {isBusinessHours() ? "Open" : "Closed"}</h5>
+        <p>To make an appointment call: (69)123 420 7890</p>
+      </Container>
+    </Jumbotron>
   );
 }
