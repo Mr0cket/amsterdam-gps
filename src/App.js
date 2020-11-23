bf5bbdd1e1a9a9cd06d14d218fbca91d355b675f
Milo Silva <milo.silva234@gmail.com>
2020-11-23 17:38:44 +0100

    refactor fetchData to handle n endpoints(ty david)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import { Route, Switch } from "react-router-dom";
 import {
@@ -9,18 +9,31 @@ import {
   PatientSignup,
   NoMatch,
 } from "./views/viewIndex";
+import getData from "./axios";
 import { NavHeader } from "./components/componentIndex";
 
 function App() {
+  const [doctorsData, setDoctorsData] = useState();
+
+  useEffect(() => {
+    getData("/doctors", setDoctorsData);
+  }, []);
   return (
     <React.Fragment>
       <NavHeader />
       {/* new syntax to avoid creating unnecessary wrapping element */}
       <Switch>
         <Route path="/doctors" component={DoctorSchedule} />
-        <Route exact path="/patients" component={Patients} />
+        <Route
+          exact
+          path="/patients"
+          component={() => <Patients doctors={doctorsData} />}
+        />
         <Route path="/patients/signup" component={PatientSignup} />
-        <Route path="patients/:patientID" component={PatientDetails} />
+        <Route
+          path="/patients/:patientID"
+          component={() => <PatientDetails doctors={doctorsData} />}
+        />
         <Route exact path="/" component={Home} />
         <Route path="/" component={NoMatch} />
       </Switch>
