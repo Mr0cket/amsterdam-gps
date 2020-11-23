3ede1e51e24ca8f0df000c23dcba82f1782029a7
Milo Silva <milo.silva234@gmail.com>
2020-11-23 12:19:35 +0100

    Implement Nav & configure bootstrap.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import react from "react";
+import React from "react";
 import "./App.css";
-import { useHistory } from "react";
 import { Route, Switch } from "react-router-dom";
 import {
   Home,
@@ -10,32 +9,22 @@ import {
   PatientSignup,
   NoMatch,
 } from "./views/viewIndex";
-import { Nav, Navbar, NavbarBrand } from "react-bootstrap";
-import { LinkContainer } from "react-router-bootstrap";
+import { NavHeader } from "./components/componentIndex";
 
 function App() {
-  const history = useHistory();
   return (
-    <div>
-      <Navbar>
-        <LinkContainer>
-          <NavbarBrand>Amsterdam GPs</NavbarBrand>
-        </LinkContainer>
-        <Nav activeKey={Window.location.pathName}>
-          <Nav.Link>Doctor Schedule</Nav.Link>
-          <Nav.Link>Patient Signup</Nav.Link>
-          <Nav.Link>Patient Database</Nav.Link>
-        </Nav>
-      </Navbar>
+    <React.Fragment>
+      <NavHeader />
+      {/* new syntax to avoid creating unnecessary wrapping element */}
       <Switch>
-        <Route exact path="/" component={Home} />
         <Route path="/doctors" component={DoctorSchedule} />
         <Route path="/patients" component={Patients} />
         <Route path="/patients/signup" component={PatientSignup} />
         <Route path="patients/:patientID" component={PatientDetails} />
+        <Route exact path="/" component={Home} />
         <Route path="/" component={NoMatch} />
       </Switch>
-    </div>
+    </React.Fragment>
   );
 }
 
---
+++ src/components/NavHeader.js
@@ -0,0 +1,17 @@
+import { useHistory } from "react-router-dom";
+import { Nav, Navbar } from "react-bootstrap";
+
+export default function NavHeader() {
+  const history = useHistory();
+  console.log(history);
+  return (
+    <Navbar bg="light" variant="light">
+      <Nav activeKey={history.location.pathname}>
+        <Nav.Link href="/">Amsterdam GPs</Nav.Link>
+        <Nav.Link href="/doctors">Doctor Schedule</Nav.Link>
+        <Nav.Link href="/patients">Patient Database</Nav.Link>
+        <Nav.Link href="/patients/signup">Patient Signup</Nav.Link>
+      </Nav>
+    </Navbar>
+  );
+}
