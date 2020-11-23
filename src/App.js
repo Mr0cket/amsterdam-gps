56bdfbef2c5d6f01a3ddbfe76a76661879d4ec61
Milo Silva <milo.silva234@gmail.com>
2020-11-23 12:19:10 +0100

    implement NavHeader Component w/ pathname

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import react from "react";
 import "./App.css";
+import { useHistory } from "react";
 import { Route, Switch } from "react-router-dom";
 import {
   Home,
@@ -7,18 +8,34 @@ import {
   Patients,
   PatientDetails,
   PatientSignup,
+  NoMatch,
 } from "./views/viewIndex";
+import { Nav, Navbar, NavbarBrand } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
 function App() {
+  const history = useHistory();
   return (
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route path="/doctors" component={DoctorSchedule} />
-      <Route path="/patients" component={Patients} />
-      <Route path="/patients/signup" component={PatientSignup} />
-      <Route path="patients/:patientID" component={PatientDetails} />
-      <Route path="/" />
-    </Switch>
+    <div>
+      <Navbar>
+        <LinkContainer>
+          <NavbarBrand>Amsterdam GPs</NavbarBrand>
+        </LinkContainer>
+        <Nav activeKey={Window.location.pathName}>
+          <Nav.Link>Doctor Schedule</Nav.Link>
+          <Nav.Link>Patient Signup</Nav.Link>
+          <Nav.Link>Patient Database</Nav.Link>
+        </Nav>
+      </Navbar>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/doctors" component={DoctorSchedule} />
+        <Route path="/patients" component={Patients} />
+        <Route path="/patients/signup" component={PatientSignup} />
+        <Route path="patients/:patientID" component={PatientDetails} />
+        <Route path="/" component={NoMatch} />
+      </Switch>
+    </div>
   );
 }
 
--- src/views/viewIndex.js
+++ src/views/viewIndex.js
@@ -3,5 +3,13 @@ import DoctorSchedule from "./DoctorSchedule";
 import Patients from "./Patients";
 import PatientDetails from "./PatientDetails";
 import PatientSignup from "./PatientSignup";
+import NoMatch from "./NoMatch";
 
-export { Home, DoctorSchedule, Patients, PatientDetails, PatientSignup };
+export {
+  Home,
+  DoctorSchedule,
+  Patients,
+  PatientDetails,
+  PatientSignup,
+  NoMatch,
+};
