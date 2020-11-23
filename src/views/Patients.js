48a2edffdee9028738eef2082f4cad2efe8553e9
Milo Silva <milo.silva234@gmail.com>
2020-11-23 14:18:13 +0100

    Flesh out Patients.js

--- src/views/Patients.js
+++ src/views/Patients.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Container, Form } from "react-bootstrap";
+import getData from "../axios";
+const endpoint = "/patients";
 
 export default function Patients() {
+  const [pageState, setPageState] = useState({
+    data: null,
+    status: "idle",
+    error: null,
+  });
+  useEffect(() => {
+    getData(endpoint, setPageState);
+  }, []);
+  let patientsList;
+  if (pageState.status === "success") {
+    patientsList = pageState.data.map();
+  }
   return (
-    <div>
+    <Container>
       <h1>Patients Database</h1>
-    </div>
+      {patientsList}
+    </Container>
   );
 }
