c50f42d48ff82bbfdd891f0cae4c7d2576285aa2
Milo Silva <milo.silva234@gmail.com>
2020-11-24 17:08:29 +0100

    Refactor axios.js

--- src/axios.js
+++ src/axios.js
@@ -2,8 +2,9 @@ import a from "axios";
 import _ from "lodash";
 const axios = a.create({
   // baseURL: "https://my-json-server.typicode.com/Codaisseur/patient-doctor-data",
-  baseURL: "http://localhost:6546",
+  baseURL: "http://localhost:6546", // must have server running to use this url.
 });
+
 export default async function fetchData(setState, ...endPoints) {
   console.log("fetching:", endPoints);
   const promises = endPoints.map((endPoint) =>
@@ -13,36 +14,17 @@ export default async function fetchData(setState, ...endPoints) {
       .catch((e) => console.error("request error:", e))
   );
 
-  const results = await Promise.all(promises);
-  if (results.length > 1) {
-    if (results.find((data) => _.isUndefined(data))) {
-      console.error(
-        "error retrieving 1 or more results, could not connect to the urls provided"
-      );
-      setState({
-        data: false, // will always be an array of results.
-        status: "fail",
-        error: "Could not connect to the urls provided",
-      });
-    } else {
-      setState({
-        data: results, // will always be an array of results.
-        status: "success",
-        error: false,
-      });
-    }
-  } else if (_.isUndefined(results[0])) {
-    console.log("error retrieving data");
+  const results = await Promise.all(promises); // returns an array of 1 or more resolved promises
+
+  // validate the requests resolved successfully
+  if (results.find((data) => _.isUndefined(data))) {
+    console.error(
+      "error retrieving 1 or more results, could not connect to the urls provided"
+    );
     setState({
       data: false,
       status: "fail",
-      error: "error retrieving data",
-    });
-  } else if (results[0]) {
-    setState({
-      data: results[0], // Will not be an array of results.
-      status: "success",
-      error: false,
+      error: "Could not connect to the urls provided",
     });
   } else if (_.isEmpty(results)) {
     console.log("error retrieving data");
@@ -50,6 +32,18 @@ export default async function fetchData(setState, ...endPoints) {
       data: false,
       status: "fail",
       error: "error retrieving data",
+    });
+  } else if (results.length > 1) {
+    setState({
+      data: results, // will always be an array of results.
+      status: "success",
+      error: false,
+    });
+  } else if (results.length === 1) {
+    setState({
+      data: results[0], // Will not be an array of results.
+      status: "success",
+      error: false,
     });
   } else {
     console.log("I shouldnt get here... but I did :/");
